5630f1ffb0c850bd63e290c6ccb3c4cf2f8cde22
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-08-08 16:35:19 +0330

    Number

---
+++ number.py
@@ -0,0 +1,8 @@
+Number1 = input ("Num1:")
+Number2 = input ("Num2:")
+Calculated_Number = Number1+Number2
+print(f"number 1 plus number 2 equals to {Calculated_Number}")
+real_num1 = int(Number1)
+real_num2 = int(Number2)
+real_calculated_number = real_num1 + real_num2 
+print(f"I just kidding the answer is {real_calculated_number}")
