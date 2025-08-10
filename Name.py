c02584aff2f8a44765afe0107b251f5642e6c2c4
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-08-10 14:00:24 +0330

    commit

---
+++ Name.py
@@ -0,0 +1,3 @@
+name = input("who are you:")
+old = input("how_old_are_you:")
+print(f"you are {name} and you are {old} years old") 
---
+++ age_calculator.py
@@ -0,0 +1,4 @@
+current_year = input("what year are we in?\n")
+born_year = input("when were you born?\n")
+age = int(current_year) - int(born_year)
+print(f"wow! you are {age} years old and after 10 years you would be {age + 10} years old")
