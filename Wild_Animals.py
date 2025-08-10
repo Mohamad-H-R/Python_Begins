a69788e4d1eaabad313005699bfe2a6a755cf1cb
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-08-10 14:00:24 +0330

    commit

---
+++ Wild_Animals.py
@@ -0,0 +1,18 @@
+# comparing 3 diffrent kind of wild animals
+Wild_Animals = {
+    "Lion":
+             {"size" : "large",
+             "habitat" : "savanna",
+              "social behavior" : "lives in groups"
+             },
+    "Leopard":
+     {"size" : "medium",
+     "habitat" : "forest, mountains",
+     "social behavior" : "mostly solitary"
+     },
+    "Wolf":
+    {"size" : "medium",
+     "habitat" : "forest,tundra",
+     "social behavior" : "lives in packs"} 
+     }
+print(Wild_Animals)
