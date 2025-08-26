11bc9e3facc4286bd289c52b2fb33a01a7f73470
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-08-26 22:40:29 +0330

    changes

---
+++ Information.py
@@ -0,0 +1,26 @@
+# 1. Create a variable named 'age' and assign it to your age as an integer.
+age = 22
+
+# 2. Create a variable called 'height" and assign it to your height in meters as a float.
+height = 1.70
+
+# 3. Create a variable called 'name' and assign it to your name as a string.
+name = "Mohamad_Hosein"
+
+# 4. Create a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not.
+is_student = True
+
+# 5. Print out all these variable.
+print(f"I am {name} and my height is {height}.\n\rI am {age} years old and am I a student? {is_student}")
+# 6. Increase the 'age' varable by 1 and print it out.
+age += 1 
+print(f"age : {age}")
+# 7. Multiply the 'height' variable by 100 to convert it to centimeters and print it out. 
+height *= 100
+print(f"height(cm):{height}")
+# 8. Add "is my name" to the 'name' variable and print it out.
+name += " is my name"
+print(name)
+# 9. Change 'Is_student' to the opposite and print it out.
+is_student = not is_student
+print(f"Is_student:{is_student}")
