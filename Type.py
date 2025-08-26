8754a9a1cd7ba7cd17e06e4ec1fc649b27d26659
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-08-26 22:19:02 +0330

    changes

---
+++ Type.py
@@ -0,0 +1,9 @@
+#  determine type value
+print(type(42))
+print(type(3.14))
+print(type("Hello, world!"))
+print(type(True))
+print(type(-7))
+print(type("42"))
+print(type(False))
+print(type(0.0))
