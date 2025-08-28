8458c0593e9ffa3722401bde34a3c75b28ace797
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-08-28 11:18:29 +0330

    changes

---
+++ create integer addition and string out.py
@@ -0,0 +1,16 @@
+# create integer addition and string output
+num = str(2 + 3)
+print(f"this is string: {num}")
+
+# string addition in the variable named new_msg(add a string to "my favourite food is ")
+new_msg = "my favourite food is " + "pizza"
+print(new_msg)
+
+# integer addition in the variable named new_sum (add 2 or more integers)
+new_sum = 0
+new_sum += 2
+print(new_sum) 
+
+# create and print a new string variable, n new_msg_2, that contatenates new_msg + a literal string
+new_msg_2 = new_msg + " and I hate Reshte Polo"
+print(new_msg_2)
