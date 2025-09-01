cb71974aa0d36f86d0a52467cc7f0331f64e64be
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-01 19:31:31 +0330

    changes

---
+++ Full name.py
@@ -0,0 +1,20 @@
+# Create a variable first_name and assignt to it your first name. 
+first_name = "Mohamad Hosein"
+
+# Create a variable last_name and assignt to it your last name. 
+last_name = "Rostami"
+
+# Create a variable full_name by concatenating first_name and last_name with a space in between.
+full_name = first_name + " " + last_name
+
+#  Print out the full_name.
+print(f"Full name: {full_name}")
+
+# Print out the length of full_name using the len()function.
+print(len(full_name))
+
+# Print out the full_name in all uppercase letters.
+print(full_name.upper())
+
+# Print out the full_name in all lowercase letters.
+print(full_name.lower())
