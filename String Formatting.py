faf082687b7e4ce86d80cb04b477b5036c970dcc
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-01 23:40:47 +0330

    changes

--- String Formatting.py
+++ String Formatting.py
@@ -1,6 +1,6 @@
 # format input() with .upper()
 # input variable fav_fruit as upper
-fav_fruit = input("What is your favorite fruit?\r\n").upper()
+fav_fruit = input("What is your favorite fruit?\r\n") #.upper()
 
 # print fav_fruit
 print(f"so your favorite fruit is {fav_fruit}")
