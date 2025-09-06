44022d280816716812816c9c186ebe59733b993d
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-06 13:23:51 +0330

    changes

---
+++ Fruits.py
@@ -0,0 +1,9 @@
+# input variable fruit_prices
+fruit_prices = {"apple": 1500, "banana": 1000, "orange": 1200}
+
+# change the price of banana
+fruit_prices["banana"] = 1100
+
+# removing apple
+fruit_prices.pop("apple")
+print(fruit_prices)
