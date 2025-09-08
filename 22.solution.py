41c5f0288eaa9ee50e6ca293db76e677405772e9
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-09 00:11:10 +0330

    changes

---
+++ 22.solution.py
@@ -0,0 +1,18 @@
+# Here's a possible solution to the assignment:
+
+
+# Create a list of top 5 favorite books
+favorite_books = ['To Kill a Mockingbird', 'The Great Gatsby', '1984', 'The Catcher in the Rye', 'Brave New World']
+
+# Print the original list of favorite books
+print("Original list of favorite books:", favorite_books)
+
+# Reverse the order of the list in place
+favorite_books.reverse()
+
+# Print the reversed list of favorite books
+print("Reversed list of favorite books:", favorite_books)
+
+# This code creates a list of favorite books, prints the original list, reverses the order of the list using the reverse()
+# method in place, and prints the reversed list. Note that we use the same variable favorite_books to store the reversed list,
+# so that we don't create a new reversed list.
