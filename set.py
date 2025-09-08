1763603c846206efb7e9c7e06824493bb8363da5
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-08 11:46:30 +0330

    changes

---
+++ set.py
@@ -0,0 +1,11 @@
+# create setA and setB
+setA = set([1, 2, 3, 4])
+setB = set([3, 4, 5, 6])
+
+# make addition
+addition_sets = setA.union(setB)
+print(addition_sets)
+
+# find intersection
+intersection_sets = setA.intersection(setB)
+print(intersection_sets)
