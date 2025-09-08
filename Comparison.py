962be6c0d3198233ae8b30f6c93f19b11347215e
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-08 19:20:54 +0330

    changes

---
+++ Comparison.py
@@ -0,0 +1,8 @@
+# input 3 number by user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# compare 3 numbers
+compare = a < b <= c
+print(f"The result of a < b <= c equals to: {compare}")
