3d9d4e4faa522bb6621a2d8d6d75a961377d6312
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-11 18:10:02 +0330

    changes

---
+++ Transform_name.py
@@ -0,0 +1,12 @@
+name = "mohamad"
+new_name = ""
+
+for ltr in name:
+    # If the letter is 'y', make it uppercase
+    if ltr.lower() == "s" or "m":
+        new_name += ltr.upper()
+    else:
+        new_name += ltr
+
+print(f"Original name: {name}")
+print(f"Transformed name: {new_name}")
