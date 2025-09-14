0765ec4f4b17803ad0d4da40844f43a406342504
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-09-14 16:56:51 +0330

    changes

---
+++ Sum.py
@@ -0,0 +1,13 @@
+def add_numbers(n1, n2):
+    """
+    this function get 2 numbers and return sum of them
+    """
+    result = n1 + n2
+    return result
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+
+print(add_numbers(num1, num2))
