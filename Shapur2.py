5a56a4966a9bc61a92561eacb4ee31d0196a5ff6
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2025-12-06 11:11:40 +0330

    changes

--- Shapur2.py
+++ Shapur2.py
@@ -8,7 +8,7 @@ while True:
     At that time, Shapur II was the king of Iran, and he managed to defeat the Arabs decisively. He then ordered that the shoulders of those captured Arabs be pierced and that they be hung from trees. This act caused such terror among the Arabs that they gave Shapur the Sassanid the title "Dhū al-Aktāf," meaning “the piercer of shoulders.”""")
         for char in text:
             print(char, end='', flush=True ), time.sleep(0.055)
-            break
+        break
     elif answer.startswith('n'):
         print("Okay, maybe next time!")
         break
